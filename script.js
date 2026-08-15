d1039170a053b296aa04f196edf5a27f82373039
tsilvalisbon <tsilvalisbon@gmail.com>
2026-08-15 21:06:22 +0100

    Nearly finished Sketchpad, still has size errors WIP

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 let mainDiv = document.querySelector("#main-div");
 
-let i = 0;
-while(i < 16 * 16)
+function createGrid(size)
 {
-    const div = document.createElement("div");
-    div.classList.add('grid-item');
-    mainDiv.appendChild(div);
-    i++;
+    mainDiv.replaceChildren();
+    let i = 0;
+    while(i < size * size)
+    {
+        const div = document.createElement("div");
+        div.classList.add('grid-item');
+    
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = "black";
+        });
+    
+        mainDiv.appendChild(div);
+        i++;
+    }
 }
+
+createGrid(16);
+
+function recreateGrid(size)
+{
+    if(!Number.isInteger(size) || size <= 0 || size > 100)
+    {
+        alert("Enter a integer between 1 and 100.");
+        return ;
+    }
+
+    createGrid(size);
+}
+
+resize.addEventListener("click", () => {
+    const answer = prompt("Enter the grid size:", "16");
+
+    if(answer === null)
+        return;
+
+    const size = Number(answer);
+    recreateGrid(size);
+})
+
+createGrid(16);
